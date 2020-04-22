19e5ac591822dd524d991138285cf32cad69262e
Zane Finner <zanefinner@pop-os.localdomain>
2020-04-22 11:52:52 -0400

    post routes declared

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+##COMMON
+Route::get('/', function(){
+    return redirect('/welcome');
 });
+Route::get('welcome', function (){
+    $message='This is a daily message!';
+    return view('welcome', ['message'=>$message]);
+});
+
+##POSTS
+Route::get('/posts/singular/{$id}/', 'PostsController@view_singular');
+Route::get('/posts/all/', 'PostsController@view_all');
+Route::get('/posts/new/', 'PostsController@show_post_view');
+Route::post('/posts/new/', 'PostsController@process_new_post');
+Route::get('/posts/new/{successful}/','PostsController@new_post_successful');
+Route::get('/posts/tag/{$tag}/', 'PostsController@view_by_tag');
+Route::get('/posts/update/{$id}', 'PostsController@show_update_view');
+Route::post('/posts/update/{$id}', 'PostsController@update_post');
+Route::get('/posts/delete/{$id}', 'PostsController@delete_post');
