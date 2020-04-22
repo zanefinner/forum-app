6ceadff337c73e15ab6f5bda33601c671af1ddbe
Zane Finner <zanefinner@pop-os.localdomain>
2020-04-22 12:13:18 -0400

    post functions created

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,5 +6,58 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    //
+    public function view_singular($id)
+    {
+        return "You want to view ".$id;
+    }
+    public function view_all()
+    {
+        return "You want to view all";
+    }
+    public function show_post_view()
+    {
+        return 'send back a template';
+    }
+    public function process_new_post()
+    {
+        /*Process*/$success = true;//or false
+        if($success)
+        {
+            //store
+            return redirect('/posts/new/success');
+        }
+        else
+        {
+            return redirect('posts/new/false');
+        }
+    }
+    public function new_post_successful($successful)
+    {
+        if($successful){
+            return 'Posted!';
+        }else{
+            $error = '';
+            redirect('/posts/new/', ['error', $error]);
+        }
+    }
+    public function view_by_tag($tag)
+    {
+        return 'Search by tag name: '.$tag;
+    }
+    public function show_update_view($id)
+    {
+        return 'Updating '.$id;
+    }
+    public function update_post(Request $re, $id)
+    {
+        //Check to see if valid
+        //update and change in database
+        return redirect('/posts/singular/'.$id, ['message', 'Success!']);
+    }
+    public function delete_post($id)
+    {
+        //make invisible in DB
+        redirect('/dashboard', ['message'=>'Deleted post successfully!']);
+        
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,12 +24,12 @@ Route::get('welcome', function (){
 });
 
 ##POSTS
-Route::get('/posts/singular/{$id}/', 'PostsController@view_singular');
+Route::get('/posts/singular/{id}/', 'PostsController@view_singular');
 Route::get('/posts/all/', 'PostsController@view_all');
 Route::get('/posts/new/', 'PostsController@show_post_view');
 Route::post('/posts/new/', 'PostsController@process_new_post');
 Route::get('/posts/new/{successful}/','PostsController@new_post_successful');
-Route::get('/posts/tag/{$tag}/', 'PostsController@view_by_tag');
-Route::get('/posts/update/{$id}', 'PostsController@show_update_view');
-Route::post('/posts/update/{$id}', 'PostsController@update_post');
-Route::get('/posts/delete/{$id}', 'PostsController@delete_post');
+Route::get('/posts/tag/{tag}/', 'PostsController@view_by_tag');
+Route::get('/posts/update/{id}', 'PostsController@show_update_view');
+Route::post('/posts/update/{id}', 'PostsController@update_post');
+Route::get('/posts/delete/{id}', 'PostsController@delete_post');
